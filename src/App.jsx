b9d3cbfa382291086a3fb981a3848f2c8a871e65
Davi Alves <cadwvk@gmail.com>
2023-02-17 16:22:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export function App() {
+
+  return (
+    <div id="app">
+      
+    </div>
+  )
+}
---
+++ src/components/Button/Button.style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 11px 38px;
+  gap: 10px;
+
+  border: 1px solid #8257E5;
+  border-radius: 6px;
+`
