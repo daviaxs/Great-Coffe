3c753a467ab13f2a70d185e72211607c33fce21d
Davi Alves <cadwvk@gmail.com>
2023-02-18 20:24:46 -0300

    added loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,75 @@
 import { Header } from "./components/Header/Header";
+import { TextMain } from "./designs/Text.style";
+import { Coffee } from "./designs/Coffee.style";
+import { Loading } from "./components/loading/Loading";
 import Light from '../src/assets/light.png';
 import CoffeeImg from '../src/assets/imgCoffee.png';
 import GreatCoder from '../src/assets/great-coder.png';
-import { TextMain } from "./designs/Text.style";
-import { Coffee } from "./designs/Coffee.style";
+import { useEffect, useState } from "react";
 import './Global.css'
 
 export function App() {
 
+  const [isLoading, setIsLoading] = useState(true)
+  const [imagesLoaded, setImagesLoaded] = useState({
+    lightLeft: false,
+    lightRight1: false,
+    lightRight2: false,
+    coffeeImg: false
+  });
+  
+  useEffect(() => {
+    if (Object.values(imagesLoaded).every((value) => value)) {
+      setIsLoading(false)
+    }
+  }, [imagesLoaded] )
+  
+  const handleOnLoad = (imageKey) => {
+    setImagesLoaded((prevState) => ({ ...prevState, [imageKey]: true }))
+  }
+  
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, [imagesLoaded]);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    window.onload = () => {
+      setIsLoading(false);
+    };
+  }, []);
+
   return (
-    <div id="app">
-      <img src={Light} id="light-left" />
-      <img src={Light} id="light-right" />
-      <img src={Light} id="light-right" />
+    <>
+      { isLoading ? (
+        <div className="loading">
+          <Loading/>
+        </div>
+      ) : (
+        <div id="app">
+          <img src={Light} className="light-left" onLoad={() => handleOnLoad('lightLeft')}/>
+          <img src={Light} className="light-right" onLoad={() => handleOnLoad('lightRight1')}/>
+          <img src={Light} className="light-right" onLoad={() => handleOnLoad('lightRight2')}/>
 
-      <Header />
+          <Header />
 
-      <TextMain className="textMain">
-        Great Coffee
-      </TextMain>
-      <TextMain className="textMainSecundary">
-       <img src={GreatCoder} alt="" />
-      </TextMain>
-      <Coffee id="coffeeImg">
-        <img src={CoffeeImg} alt="" />
-      </Coffee>
+          <TextMain className="textMain">
+            Great Coffee
+          </TextMain>
 
-    </div>
+          <TextMain className="textMainSecundary">
+            <img src={GreatCoder} alt="" />
+          </TextMain>
+
+          <Coffee id="coffeeImg" >
+            <img src={CoffeeImg} alt="" onLoad={() => handleOnLoad('coffeeImg')} />
+          </Coffee>
+        </div>
+      )}
+    </>
   )
 }
---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+export function Loading() {
+  return (
+    <div class="sk-chase">
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+    </div>
+
+  )
+}
